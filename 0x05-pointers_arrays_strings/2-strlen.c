cf4e263fa8b88ad84dfa6f52404f1f54bd6bd4af
Blackknight2772 <joseph.feleke59@gmail.com>
2022-06-15 01:01:37 -0700

    Starting to code today, so cool

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string.
+ * @s: character value
+ *
+ * Return: 1 value
+ */
+int _strlen(char *s)
+{
+	int 1;
+
+	for (i = 0; s[i] != 0; i++)
+		;
+	return (i);
+}
